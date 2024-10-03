1cc181f686874a6e619d7a73fa9399f19fdacc11
Yurizal Susanto <rizalsagi@gmail.com>
2024-10-03 15:10:01 +0700

    Register handler

--- app/main.go
+++ app/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"app/post"
+	"app/user"
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func NotImplemented(w http.ResponseWriter, r *http.Request) {
@@ -15,16 +20,20 @@ func NotImplemented(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	db := initDB()
+	userService := user.NewService(db)
+	postService := post.NewService(db)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", NotImplemented)
-	mux.HandleFunc("POST /register", NotImplemented)
-	mux.HandleFunc("POST /login", NotImplemented)
+	mux.HandleFunc("POST /register", userService.RegisterHandler())
+	mux.HandleFunc("POST /login", userService.LoginHandler())
 
-	mux.HandleFunc("GET /posts", NotImplemented)
-	mux.HandleFunc("POST /posts", NotImplemented)
-	mux.HandleFunc("GET /posts/{id}", NotImplemented)
-	mux.HandleFunc("PUT /posts/{id}", NotImplemented)
-	mux.HandleFunc("DELETE /posts/{id}", NotImplemented)
+	mux.HandleFunc("GET /posts", postService.PostsHandler())
+	mux.HandleFunc("POST /posts", user.TokenMiddleware(postService.CreatePostHandler()))
+	mux.HandleFunc("GET /posts/{id}", postService.PostHandler())
+	mux.HandleFunc("PUT /posts/{id}", user.TokenMiddleware(postService.UpdatePostHandler()))
+	mux.HandleFunc("DELETE /posts/{id}", user.TokenMiddleware(postService.DeletePostHandler()))
 
 	mux.HandleFunc("GET /posts/{id}/comments", NotImplemented)
 	mux.HandleFunc("POST /posts/{id}/comments", NotImplemented)
@@ -52,4 +61,18 @@ func main() {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatalf("server shutdown returned err: %v", err)
 	}
+}
+
+func initDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:abc123@tcp(db:3306)/appdb?parseTime=true&loc=Asia%2FJakarta")
+	if err != nil {
+		panic(err)
+	}
+
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
 }
